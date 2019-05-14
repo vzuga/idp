effccba999e90817aa464ebc53e0bd354fccd7a9
vzuga <vladzuga@gmail.com>
2019-05-15 02:17:14 +0300

    Add initial migration

---
+++ OnlineStore/Migrations/20190514231636_InitialCreate.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineStore.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderProducts",
+                columns: table => new
+                {
+                    OrderId = table.Column<decimal>(type: "numeric(18, 0)", nullable: false),
+                    ProductId = table.Column<decimal>(type: "numeric(18, 0)", nullable: false),
+                    Quantity = table.Column<decimal>(type: "numeric(18, 0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProducts", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "numeric(18, 0)", nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Email = table.Column<string>(maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Address = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "numeric(18, 0)", nullable: false),
+                    UserId = table.Column<decimal>(type: "numeric(18, 0)", nullable: false),
+                    Status = table.Column<string>(maxLength: 50, nullable: false),
+                    PlacedDate = table.Column<DateTime>(type: "date", nullable: false),
+                    DeliveredDate = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_OrderProducts",
+                        column: x => x.Id,
+                        principalTable: "OrderProducts",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "OrderProducts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
