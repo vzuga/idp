5d500bbcf6a59c50d55952287224ad1952c3f721
vzuga <vladzuga@gmail.com>
2019-05-15 10:01:14 +0300

    Use data seeder

---
+++ OnlineStore/DataSeeder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Models;
+
+namespace OnlineStore
+{
+    public static class DataSeeder
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using (var context = new OnlineStoreContext())
+            {
+                if (!context.Database.EnsureCreated())
+                {
+                    context.Database.Migrate();
+                }
+            }
+        }
+    }
+}
